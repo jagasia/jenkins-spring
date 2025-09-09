258bd14dd7354227a02f54d6ba227f15f49b766c
jagasia <rjagadeeswaran@Yahoo.com>
2025-09-09 08:31:43 +0530

    first commit

---
+++ hello-service/src/main/java/com/empower/demo/HelloServiceApplication.java
@@ -0,0 +1,21 @@
+package com.empower.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloServiceApplication {
+
+	@GetMapping
+	public String home() {
+		return "Welcome home";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+
+}
